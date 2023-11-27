b4f967ab6d6288edc315f8b9cd32f9cb1af89d44
sarvagya chaudhary <sarvagya.chaudhary@amplussolar.com>
2023-11-27 13:18:30 +0530

    importing day1 in main.py

---
+++ day1.py
@@ -0,0 +1,44 @@
+#basically comment  start with # and python ignore them during execution....
+a=10
+b=20
+print(a+b)
+#line four print sum of a and b but if i comment it python ignore this 
+# print(a+b)  i comment this 
+print ('this is comment that wrriten in line 1,5 and 6')
+
+
+#variabels 
+#variables thats use to store dataor values on behalf of user  it can be string, number,list,dictionary etc
+a=10
+b=20
+print(a)
+print(b)
+#note 
+# 1-variable name can not be start from number  like 123a 
+# 2-variable can start with underscore ex _123a
+# 3-variable can not be start with special characer like @,#,$,%,&,. etc
+# variable never have space between letter and words like a b=10,abc abc=20,a b c=10
+#can use _ at the place of blank space.... abc_abc=10
+
+#important  note 
+#variable stored in python or make memory in system on the..
+#basis of value stored in variable not depend of variable name 
+
+a=10
+b=10
+print(id(a)) #140715070448712
+print(id(a)) #140715070448712
+
+
+#here variable name different ,value are same and the address are appear same when print it .
+a=20
+b=30
+print(id(a))   #140715070449032
+print(id(b))   #140715070449352
+
+#here variable name different ,value are differen and the address are appear also different when print it .
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-#basically comment  start with # and python ignore them during execution....
-a=10
-b=20
-print(a+b)
-#line four print sum of a and b but if i comment it python ignore this 
-# print(a+b)  i comment this 
-print ('this is comment that wrriten in line 1,5 and 6')
-
-
-#variabels 
-#variables thats use to store dataor values on behalf of user  it can be string, number,list,dictionary etc
-a=10
-b=20
-print(a)
-print(b)
-
+import day1
